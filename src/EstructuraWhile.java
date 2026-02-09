5aa1b6dd98fe6220711f696cd10ac097d2d43fd3
jitv-dev <javier.torresvalenz@gmail.com>
2026-02-09 20:54:27 -0300

    While suma y promedio

---
+++ src/EstructuraWhile.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class EstructuraWhile {
+    // Mostrar por consola los numeros del 1 al 100
+//    public static void main (String[] args){
+//        int x;
+//        x=1;
+//        while (x <= 100) {
+//            System.out.print(x + " - ");
+//            x = x + 1;
+//        }
+//    }
+
+    // While por teclado se ingresen 10 numeros, luego mostrar suma y promedio
+//    public static void main (String[] args){
+//        Scanner teclado = new Scanner(System.in);
+//
+//        int x;
+//        float numIngresado, acumulador, promedio;
+//        x = 1;
+//        acumulador = 0;
+//
+//        while (x <= 10) {
+//            System.out.println("Ingrese un número (" + x + "/10): ");
+//            numIngresado = teclado.nextFloat();
+//            acumulador = acumulador + numIngresado;
+//            x = x + 1;
+//        }
+//
+//        promedio = acumulador / 10;
+//
+//        System.out.println("La suma total es: " + acumulador);
+//        System.out.println("El promedio es: " + promedio);
+//    }
+
+    // Numeros de empleados, sueldos entre 100 y 500
+    // Informar empleados que cobren entre 100 y 300 || + de 300 || + de 400 || cuanto es el total que pagare
+    public static void main (String[] args){
+
+    }
+}
