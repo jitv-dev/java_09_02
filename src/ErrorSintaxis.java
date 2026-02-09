e16da5e3182d57661bf208886aa0d209b88dc152
jitv-dev <javier.torresvalenz@gmail.com>
2026-02-09 19:34:18 -0300

    Error sintaxis

--- src/ErrorSintaxis.java
+++ src/ErrorSintaxis.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class ErrorSintaxis {
+    // calcular la superficie de un cuadrado
+    public static void main (String[] args){
+        // El usuario ingresa informacion
+        Scanner teclado = new Scanner(System.in);
+        // Variables
+        int lado;
+        int superficie;
+
+        // Solicitd al usuario que ingrese los datos
+        // Si este fuera system en minuscula arrojaria error
+        System.out.println("Ingrese el valor del lado del cuadrado: ");
+        lado = teclado.nextInt();
+        superficie = lado * lado;
+        System.out.println("La superficie del cuadrado es: ");
+        System.out.println(superficie);
+    }
 }
+
+// Error de sintaxis es no respetar CamelCase o hacer calculos errados
