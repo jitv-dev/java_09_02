e6dc0f295c3ffbd897868a0bf0038dcaad5d951f
jitv-dev <javier.torresvalenz@gmail.com>
2026-02-09 21:11:48 -0300

    Ejemplos while

--- src/EstructuraWhile.java
+++ src/EstructuraWhile.java
@@ -36,6 +36,37 @@ public class EstructuraWhile {
     // Numeros de empleados, sueldos entre 100 y 500
     // Informar empleados que cobren entre 100 y 300 || + de 300 || + de 400 || cuanto es el total que pagare
     public static void main (String[] args){
+        Scanner teclado = new Scanner(System.in);
 
+        //Variables
+        int empleados, cont1, cont2, cont3, x;
+        float sueldo, gastos;
+        x = 1;
+        cont1 = 0;
+        cont2 = 0;
+        cont3 = 0;
+        gastos = 0;
+
+        System.out.println("Ingrese el numero de empleados: ");
+        empleados = teclado.nextInt();
+
+        while (x <= empleados) {
+            System.out.println("Ingrese el sueldo del empleado: $");
+            sueldo = teclado.nextFloat();
+            if (sueldo <= 300){
+                cont1 = cont1 + 1;
+            } else if (sueldo > 300 && sueldo <= 400){
+                cont2 = cont2 + 1;
+            } else {
+                cont3 = cont3 + 1;
+            }
+            gastos =  gastos + sueldo;
+            x = x +1;
+        }
+
+        System.out.println("Cantidad de empleados con sueldos entre 100 y 300: " + cont1);
+        System.out.println("Cantidad de empleados con sueldos entre 300 y 400: " + cont2);
+        System.out.println("Cantidad de empleados con sueldos mayores a 400: " + cont3);
+        System.out.println("Total a pagar: " + gastos);
     }
 }
